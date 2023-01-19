2840ac4669ddaa70513393db07b7be91e56d5659
GonzaloBordas <113022730+GonzaloBordas@users.noreply.github.com>
2023-01-19 13:29:51 +0100

    Update viogenero.sql

--- backend/src/database/viogenero.sql
+++ backend/src/database/viogenero.sql
@@ -1,12 +1,12 @@
 CREATE DATABASE IF NOT EXISTS `viogenero` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-CREATE TABLE `chatbot` (
+CREATE TABLE IF NOT EXISTS `chatbot` (
   `id_question` int NOT NULL AUTO_INCREMENT,
   `question` varchar(300) NOT NULL,
   `target` int NOT NULL,
   PRIMARY KEY (`id_question`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE `chatbot_training` (
+CREATE TABLE IF NOT EXISTS `chatbot_training` (
   `id_question` int NOT NULL AUTO_INCREMENT,
   `question` varchar(300) NOT NULL,
   `target` int NOT NULL,
